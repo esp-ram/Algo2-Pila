a6c4929960db06e4933cf42b97f4a814da20cb6c
esp-ram <48606743+esp-ram@users.noreply.github.com>
2019-09-15 17:17:29 -0300

    Add files via upload

--- pila.c
+++ pila.c
@@ -1,6 +1,8 @@
 #include "pila.h"
 #include <stdlib.h>
 #define TAM_INICIAL 4
+#define FACTOR_REDIMENSION 2
+
 
 /* Definición del struct pila proporcionado por la cátedra.
  */
@@ -21,7 +23,7 @@ pila_t* pila_crear(void){
     if (nueva == NULL) {
         return NULL;
     }
-    nueva->datos = malloc(TAM_INICIAL* 8 );
+    nueva->datos = malloc(TAM_INICIAL* sizeof(void*));
 
     if (nueva->datos == NULL){
         free(nueva);
@@ -46,10 +48,7 @@ bool redimensionar(pila_t* pila, size_t nuevo_tam){
 
 
 bool pila_esta_vacia(const pila_t *pila){
-    if (pila->cantidad == 0){
-        return true;
-    }
-    return false;
+    return(pila->cantidad == 0);
 }
 
 
@@ -69,7 +68,7 @@ void* pila_ver_tope(const pila_t *pila){
 
 bool pila_apilar(pila_t *pila, void* valor){
     if (pila->cantidad == pila->capacidad){
-        if (redimensionar(pila,pila->capacidad * 2) == false){
+        if (redimensionar(pila,pila->capacidad * FACTOR_REDIMENSION) == false){
             return false;
             }
         }
@@ -86,7 +85,7 @@ void* pila_desapilar(pila_t *pila){
     void* dato_desapilado = pila->datos[pila->cantidad - 1];
     pila->cantidad -= 1;
     if ((pila->cantidad * 4 <= pila->capacidad) && (pila->capacidad > TAM_INICIAL)){
-        redimensionar(pila,pila->capacidad / 2);
+        redimensionar(pila,pila->capacidad / FACTOR_REDIMENSION);
     }
     return (dato_desapilado);
 }
--- pruebas_alumno_pila.c
+++ pruebas_alumno_pila.c
@@ -3,13 +3,14 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdbool.h>
-#define ELEMENTOS_PRUEBA_GRANDE 1100
+#define ELEMENTOS_PRUEBA_VOLUMEN 1100
+
 /* ******************************************************************
  *                   PRUEBAS UNITARIAS ALUMNO
  * *****************************************************************/
 
-bool apilar_grande(pila_t* pila, int* vector_pruebas){
-    for(int i = 0; i < ELEMENTOS_PRUEBA_GRANDE; i++){
+bool apilar_volumen(pila_t* pila, int* vector_pruebas){
+    for(int i = 0; i < ELEMENTOS_PRUEBA_VOLUMEN; i++){
         if (pila_apilar(pila,&vector_pruebas[i]) == false){
             return false;
         }
@@ -18,8 +19,8 @@ bool apilar_grande(pila_t* pila, int* vector_pruebas){
 }
 
 
-bool desapilar_grande(pila_t* pila, int* vector_pruebas){
-    int limite = ELEMENTOS_PRUEBA_GRANDE - 1 ;
+bool desapilar_volumen(pila_t* pila, int* vector_pruebas){
+    int limite = ELEMENTOS_PRUEBA_VOLUMEN - 1 ;
     while (pila_esta_vacia(pila) == false){
         if (pila_ver_tope(pila) != &vector_pruebas[limite]){
             return false;
@@ -30,44 +31,95 @@ bool desapilar_grande(pila_t* pila, int* vector_pruebas){
     return true;
 }
 
-
-
-void pruebas_pila_alumno() {
+void prueba_crear_destruir(){
     pila_t* pi = pila_crear();
-    int a = 2;
-    int b = 5;
-    int c = 7;
-    int d = 9;
+    pila_destruir(pi);
+}
 
-    print_test("desapilar pila vacia:",pila_desapilar(pi) == NULL);
-    print_test("ver tope de pila vacia:",pila_ver_tope(pi) == NULL);
-    print_test("ver si la pila esta vacia:",pila_esta_vacia(pi) == true);
+
+void prueba_volumen(){
+    pila_t* pi = pila_crear();
+    int vector_temporal[ELEMENTOS_PRUEBA_VOLUMEN];
+    print_test("apilando muchos elementos",apilar_volumen(pi,vector_temporal) == true);
+    print_test("despilando los elementos de la pila y verificando tope de pila",desapilar_volumen(pi,vector_temporal) == true);
+    print_test("pila vacia? ",pila_ver_tope(pi) == NULL);
+    pila_destruir(pi);
+}
+
+
+void prueba_null(){
+    pila_t* pi = pila_crear();
     print_test("apilo NULL",pila_apilar(pi,NULL) != false);
     print_test("desapilo NULL",pila_desapilar(pi) == NULL);
-    //apilar los tres elementos
+    print_test("pila vacia? ",pila_ver_tope(pi) == NULL);
+    pila_destruir(pi);
+}
+
+
+void prueba_pila_nueva(){
+    pila_t* pi = pila_crear();
+    print_test("pila vacia? ",pila_ver_tope(pi) == NULL);
+    print_test("desapilar pila vacia:",pila_desapilar(pi) == NULL);
+    print_test("ver tope de pila vacia:",pila_ver_tope(pi) == NULL);
+    pila_destruir(pi);
+}
+
+
+void prueba_apilar_desapilar(){
+    int a = 2;
+    int b = 6;
+    int c = 374;
+    int d = 52;
+    pila_t* pi = pila_crear();
+    print_test("apilo elemento ",pila_apilar(pi,&a) != false);
+    print_test("se verifica el tope de la pila",*(int*)pila_ver_tope(pi) == a);
+    print_test("apilo elemento ",pila_apilar(pi,&b) != false);
+    print_test("se verifica el tope de la pila",*(int*)pila_ver_tope(pi) == b);
+    print_test("apilo elemento ",pila_apilar(pi,&c) != false);
+    print_test("se verifica el tope de la pila",*(int*)pila_ver_tope(pi) == c);
+    print_test("apilo elemento ",pila_apilar(pi,&d) != false);
+    print_test("se verifica el tope de la pila",*(int*)pila_ver_tope(pi) == d);
+    print_test("desapilo un elemento:",*(int*)pila_desapilar(pi) == d);
+    print_test("se verifica el tope de la pila",*(int*)pila_ver_tope(pi) == c);
+    print_test("desapilo un elemento:",*(int*)pila_desapilar(pi) == c);
+    print_test("se verifica el tope de la pila",*(int*)pila_ver_tope(pi) == b);
+    print_test("desapilo un elemento:",*(int*)pila_desapilar(pi) == b);
+    print_test("se verifica el tope de la pila",*(int*)pila_ver_tope(pi) == a);
+    print_test("desapilo un elemento:",*(int*)pila_desapilar(pi) == a);
+    printf("comprobacion si la pila funciona como recien creada\n");
+    print_test("verificacion tope de pila ",pila_ver_tope(pi) == NULL);
+    print_test("intento desapilar la pila ",pila_desapilar(pi) == NULL);
+    print_test("veo si la pila esta vacia ",pila_esta_vacia(pi) == true);
+    pila_destruir(pi);
+}
+
+
+void destruccion_elementos(){
+    int a = 2;
+    int b = 5;
+    pila_t* pi = pila_crear();
     print_test("apilo elemento ",pila_apilar(pi,&a) != false);
     print_test("apilo elemento ",pila_apilar(pi,&b) != false);
-    print_test("apilo elemento ",pila_apilar(pi,&c) != false);
-    print_test("apilo elemento ",pila_apilar(pi,&d) != false);
-    print_test("apilo elemento ",pila_apilar(pi,&a) != false);
-    print_test("se verifica el tope de la pila",*(int*)pila_ver_tope(pi) == 2);
-    print_test("desapilo un elemento:",*(int*)pila_desapilar(pi) == 2);
-    print_test("verificacion si esta vacia",pila_esta_vacia(pi) == false);
-    //despilar toda la pila
-    print_test("desapilo un elemento:",*(int*)pila_desapilar(pi) == 9);
-    print_test("desapilo un elemento:",*(int*)pila_desapilar(pi) == 7);
-    print_test("desapilo un elemento:",*(int*)pila_desapilar(pi) == 5);
-    print_test("desapilo un elemento:",*(int*)pila_desapilar(pi) == 2);
-    print_test("verificacion tope de pila",pila_ver_tope(pi) == NULL);
-    print_test("intento desapilar la pila",pila_desapilar(pi) == NULL);
-    print_test("veo si la pila esta vacia",pila_esta_vacia(pi) == true);
-    //prueba con un numero grande de elementos definidos anteriormente
-    int vector_temporal[ELEMENTOS_PRUEBA_GRANDE];
-    print_test("apilando muchos elementos",apilar_grande(pi,vector_temporal));
-    print_test("despilando los elementos de la pila y verificando tope de pila",desapilar_grande(pi,vector_temporal) == true);
-    print_test("pila vacia? ",pila_ver_tope(pi) == NULL);
-    print_test("intento de desapilar pila vacia ",pila_desapilar(pi) == NULL);
-    printf("destruir pila con elementos\n");
-    pila_apilar(pi,&a);
     pila_destruir(pi);
+}
+
+
+void pruebas_pila_alumno(){
+    printf("prueba creacion y destruccion de pila:\n");
+    prueba_crear_destruir();
+
+    printf("\nprueba comportamiento de pila vacia:\n");
+    prueba_pila_nueva();
+
+    printf("\nprueba apilar elemento NULL:\n");
+    prueba_null();
+
+    printf("\nprueba de apilado y desapilado de elementos:\n");
+    prueba_apilar_desapilar();
+
+    printf("\nprueba con %d elementos:\n",ELEMENTOS_PRUEBA_VOLUMEN);
+    prueba_volumen();
+
+    printf("\nprueba destruccion de pila con elementos:\n");
+    destruccion_elementos();
 }
